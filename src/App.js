c4a79c5e92a9f937ed9b93bba8ba5d0edbab227e
Emma Kamarainen <emma.kamarainen@thoughtworks.com>
2022-12-30 16:21:18 +0200

    refactor, className changes

--- src/App.js
+++ src/App.js
@@ -1,108 +1,18 @@
-import React, { useState } from 'react'
-import './App.css'
-import Card from './components/UI/Card'
-import Chest from './components/Chest'
-import ChestContent from './components/ChestContent'
+import Store from "./components/Store/Store";
+import Card from "./components/UI/Card";
+import classes from "./App.css";
+import Terrain from "./components/Treasure/Terrain";
 
 function App() {
-  const [woodSelected, selectedWoodenChest] = useState(false)
-  const [silverSelected, selectedSilverChest] = useState(false)
-  const [goldSelected, selectedGoldChest] = useState(false)
-
-  const ShowTreasureHandler = () => {
-    selectedWoodenChest(false)
-    selectedSilverChest(false)
-    selectedGoldChest(false)
-  }
-  const selectWoodChest = () => {
-    selectedWoodenChest(true)
-    selectedSilverChest(false)
-    selectedGoldChest(false)
-    return woodSelected
-  }
-
-  const selectSilverChest = () => {
-    selectedWoodenChest(false)
-    selectedSilverChest(true)
-    selectedGoldChest(false)
-    return silverSelected
-  }
-
-  const selectGoldChest = () => {
-    selectedWoodenChest(false)
-    selectedSilverChest(false)
-    selectedGoldChest(true)
-    return goldSelected
-  }
-
-  const treasureChests = [
-    {
-      id: 'wood',
-      type: 'Wood',
-      upperLimit: 100,
-      lowerLimit: 0,
-      selected: woodSelected,
-    },
-    {
-      id: 'silver',
-      type: 'Silver',
-      upperLimit: 1000,
-      lowerLimit: 100,
-      selected: silverSelected,
-    },
-    {
-      id: 'gold',
-      type: 'Gold',
-      upperLimit: 1500,
-      lowerLimit: 1000,
-      selected: goldSelected,
-    },
-  ]
-
-  const showChests = () => {
-    if (
-      [woodSelected, silverSelected, goldSelected].every(
-        (stat) => stat === false,
-      )
-    )
-      return (
-        <div>
-          <Chest
-            chestType={treasureChests[0].type}
-            selected={selectWoodChest}
-          />
-          <Chest
-            chestType={treasureChests[1].type}
-            selected={selectSilverChest}
-          />
-          <Chest
-            chestType={treasureChests[2].type}
-            selected={selectGoldChest}
-          />
-        </div>
-      )
-    return (
-      <div>
-        <ChestContent
-          showTreasure={ShowTreasureHandler}
-          treasure={treasureChests}
-        />
-      </div>
-    )
-  }
-
   return (
-    <Card
-      className={`App App${woodSelected ? '-wood' : '-home'} App${
-        silverSelected ? '-silver' : '-home'
-      } App${goldSelected ? '-gold' : '-home'}`}
-    >
-      <Card className="App-header">
-        <p>Treasure Chest</p>
-        {showChests()}
+    <div className={classes['App-header']}>
+      <Card>
+        <Terrain/>
+        
+        <Store />
       </Card>
-    </Card>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
